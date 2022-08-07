0914d24c3c5b14a9d207a5b26dafa1b0365db6eb
冨永桃 <momotominaga@tominagamomonoMacBook-Pro.local>
2022-08-07 11:00:12 +0900

    リロードボタン作成

--- janken/janken.js
+++ janken/janken.js
@@ -1,34 +1,42 @@
-
-
 function ChooseHand(playerHand) {
   // 乱数0~3
   let randomNumber = Math.floor(Math.random() * 3);
 
   // 相手の手の色を変える。
   let opponent = document.getElementById(`opponent_${randomNumber}`);
-  let opponentHandImage = document.getElementById(`opponent_hand_img_${randomNumber}`);
+  let opponentHandImage = document.getElementById(
+    `opponent_hand_img_${randomNumber}`
+  );
   opponent.classList.add("Hands__list-item__opponent-hand");
   opponentHandImage.classList.add("show");
 
   // 自分のての色を変える
   let player = document.getElementById(`player_${playerHand}`);
-  let playerHandImage = document.getElementById(`player_hand_img_${playerHand}`);
+  let playerHandImage = document.getElementById(
+    `player_hand_img_${playerHand}`
+  );
   player.classList.add("Hands__list-item__player-hand");
   playerHandImage.classList.add("show");
 
   if (playerHand === randomNumber) {
-    result = "あいこ"
-  } else if (playerHand === 0 && randomNumber === 1 || playerHand === 1 && randomNumber === 2 || playerHand === 2 && randomNumber === 0) {
-    result = "君の勝ち！"
+    result = "あいこ";
+  } else if (
+    (playerHand === 0 && randomNumber === 1) ||
+    (playerHand === 1 && randomNumber === 2) ||
+    (playerHand === 2 && randomNumber === 0)
+  ) {
+    result = "君の勝ち！";
   } else {
-    result = "お前の負けm9(^Д^)"
+    result = "お前の負けm9(^Д^)";
   }
-  
+// 一度じゃんけんをしたらリロードしないと押せなくする
   document.getElementById("jankenGame").style.pointerEvents = "none";
-  
-  console.log(result);
 
-  let jankenResult = document.getElementById("jankenResult");
-  jankenResult.insertAdjacentHTML("beforeend",result);
-
+  let jankenresult = document.getElementById("jankenresult");
+  jankenresult.insertAdjacentHTML("beforeend", result);
 }
+// リロードボタン
+const reloadBtn = document.getElementById("reloadBtn");
+reloadBtn.addEventListener("click", function () {
+  location.reload();
+});
